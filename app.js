cc8bf92fa5cd629faafea61bffa6821caf433a92
sukhwinder-tech <ramanpaul6283@gmail.com>
2023-04-08 17:30:39 +0530

    changes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require("./db/conn");
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const PORT = 6001
+const PORT = process.env.PORT || 6001
 const router = require("./Routes/router")
 
 
